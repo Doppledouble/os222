1fc0234d98b0c7f809bed15114eee8c51e9a2fc0
Doppledouble~ <ian.suryadi@ui.ac.id~>
2022-09-25 12:00:05 +0700

    new Update

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="720B08264EF92745"
+REC2="B1099382E9F59510"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
